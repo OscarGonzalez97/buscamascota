ab9469f16e772b8c018c5c7c092285ac503c930b
OscarGonzalez97 <oscaren_gonz0912@hotmail.com>
2020-09-28 14:56:23 -0400

    agrego messages

--- buscamascota/settings.py
+++ buscamascota/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -132,3 +133,14 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'app/static/'),
 )
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
+FILE_UPLOAD_HANDLER=["django.core.files.uploadhandler.MemoryFileUploadHandler",
+ "django.core.files.uploadhandler.TemporaryFileUploadHandler"]
