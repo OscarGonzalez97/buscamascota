fda312bd7466ed0f58d5bee710060452438f15fc
OscarGonzalez97 <oscaren_gonz0912@hotmail.com>
2020-10-04 16:01:59 -0300

    estableci rutas para utilizar media

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from app import views
-
+from buscamascota import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+ 
 urlpatterns = [
     path('', views.index, name='index'),
     path('colaborar/', views.colaborate, name='colaborar'),
@@ -8,4 +11,8 @@ urlpatterns = [
     path('publicar/', views.publish, name='publicar'),
     path('terminos/', views.terms, name='terminos'),
     path('licencia/', views.license, name='licencia'),
+    path('exito/<int:report_id>', views.success, name='success'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
