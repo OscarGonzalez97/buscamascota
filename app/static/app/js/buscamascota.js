ebc4432cb3c116e0b95bd5823a5b4e6095b30498
OscarGonzalez97 <oscaren_gonz0912@hotmail.com>
2020-11-24 13:51:58 -0300

    se quita mapa del index y se agrega funcion para agregar marcadores al mapa

--- app/static/app/js/buscamascota.js
+++ app/static/app/js/buscamascota.js
@@ -1,37 +1,65 @@
 
-function initMap(mapDivId,isIndex) {
+function initMap(mapDivId) {
     /**
      Initialize leaflet Map in the DOM
      @param mapDivName: ID of the map's div in the DOM
      @param isIndex: boolean to know if the map is Index page
       **/
-    var map;
-    if (isIndex){
-        map = L.map(mapDivId, {
-            scrollWheelZoom: false,
-            fullscreenControl: true,
-            // OR
-            fullscreenControl: {
-                pseudoFullscreen: false // if true, fullscreen to page width and height
-            },
-            minZoom: 2
-        }).setView([0, 0], 2);
-    }
-    else{
-        map = L.map(mapDivId, {
-            fullscreenControl: true,
-            // OR
-            fullscreenControl: {
-                pseudoFullscreen: false
-            },
-            minZoom: 2
-        }).setView([-23.3165935,-58.1693445], 6); //Paraguay's coordenates
-    }
+    let map;
+    map = L.map(mapDivId, {
+        scrollWheelZoom: true,
+        fullscreenControl: true,
+        // OR
+        fullscreenControl: {
+            pseudoFullscreen: false
+        },
+        minZoom: 2
+    }).setView([-23.3165935,-58.1693445], 6); //Paraguay's coordenates
+    
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     return map;
+}
+
+function addMarkers(reports, map, markers) {
+
+    /***
+    map: variable containing the map
+    markers: variable containing the group of markers
+    ***/
+    
+    // Create markers
+    let inst_lat, inst_lng; 
+    let arr_pos = [];
+    let new_lat, new_lng;
+
+    for (i=0; i < reports.length; i++) {
+        lat = reports[i].latitude;
+        lon = reports[i].longitude;
+
+        let leafletMarker = L.marker([lat,lon]);
+        
+        leafletMarker.bindPopup(generateInfoWindowContent(reports[i]), {
+            minWidth : 200
+        } ).openPopup();
+        
+        markers.addLayer(leafletMarker);
+    }
+
+    //leaflet cluster added to map
+    map.addLayer(markers);
+}
+
+function generateInfoWindowContent(report_info) {
+    let content = "";
+    content = "<p class='text-center text-uppercase h4' style='color: red;'>" + report_info.report_type + "</p>" 
+    content += "<img src='"+report_info.picture+"' alt='mascota' style='width: 100%; height: 100%;'>";
+    if (report_info.phone != null) 
+        content += "<p class='text-center h6'> Contacto: " + report_info.phone + "</p>";
+    content += "<p class='text-center text-dark'> <a class=\"btn btn-warning mt-1 amarillo\" href=\"http://buscamascota.org/reporte/"+ report_info.id +"\"> Ver reporte completo </a>  </p>"
+    return content
 }
 
 function completeUbicationWidgets(latitude, longitude){
@@ -81,5 +109,19 @@ function enableSubmit(){
     }
     else{
         document.getElementById("submitButton").disabled = true;
+    }
+}
+
+function changeView(){
+    if(document.getElementById("viewMap").checked){
+        document.getElementById("divMapView").style.display = 'block';
+        document.getElementById("divListView").style.display = 'none';
+    }
+    else if (document.getElementById("viewList").checked){
+        document.getElementById("divMapView").style.display = 'none';
+        document.getElementById("divListView").style.display = 'block';
+    }
+    else{
+
     }
 }
