b6f1c78759710c94b5d9201bf89493c9bcff883f
OscarGonzalez97 <oscaren_gonz0912@hotmail.com>
2020-12-04 04:10:30 -0300

    para que pueda buscar un reporte y quitar el allowed

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Report
 
-# Register your models here.
+class ReportAdmin(admin.ModelAdmin):
+    search_fields = ['id']
+
+admin.site.register(Report, ReportAdmin)
