4ddc552a50dfc2f729995233351cc30c2eac42ac
Franco Pecci <fapeccisan@gmail.com>
2023-06-20 16:12:59 -0400

    update: se cambia la url de guardar adopciones

--- app/urls.py
+++ app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     re_path(r'^reporte/(?P<report_id>[0-9]+)$', views.report, name='report'),
     path('lista_reportes', views.report_list, name="lista_reportes"),
     re_path(r'^adopcion/(?P<adopt_id>[0-9]+)$', views.adopt, name='adopt'),
-    path('detalle_adopcion', views.PetAdoptionModel, name="detalle_adopcion"),  #Url para guardar una adopción
+    path('adopciones-publicar/', views.publicar_adopcion, name="adopciones-publicar"),
 
     #API
     path('api/reportes/', views.ReportListAPIView.as_view(), name='report_list_json')
